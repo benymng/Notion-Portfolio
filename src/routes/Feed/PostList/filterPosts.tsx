17f53b382f98852b5248fc6dcfa04a19f12a25bc
Benjamin Ng <benymng@gmail.com>
2025-04-25 09:41:36 -0400

    Update avatar and add default tag

--- src/routes/Feed/PostList/filterPosts.tsx
+++ src/routes/Feed/PostList/filterPosts.tsx
@@ -12,7 +12,7 @@ interface FilterPostsParams {
 export function filterPosts({
   posts,
   q,
-  tag = undefined,
+  tag = "⭐",
   category = DEFAULT_CATEGORY,
   order = "desc",
 }: FilterPostsParams): TPost[] {
